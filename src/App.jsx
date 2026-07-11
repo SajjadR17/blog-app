9e98082a751f3a3bb0f3040afd842adfeb5a986f
Sajjad Roohandeh <Roohandehsredi6@gmail.com>
2026-07-11 21:08:47 +0330

    Fix Bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import SideBar from "./components/SideBar";
 import Essays from "./pages/Essays";
 import Journal from "./pages/Journal";
 import About from "./pages/About";
